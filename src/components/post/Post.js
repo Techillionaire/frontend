9c83b260c17244a4ba35b6bd569aae285a3e57d0
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-05-10 07:18:17 +0100

    consuming APIs

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,26 +2,28 @@ import './post.css'
 
 import React from 'react'
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <div className='post'>
-        <img className='postImg' src='https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
-
-        <div className='postInfo'>
+      {post.photo && (
+        <img 
+          className='postImg' 
+          src={post.photo} 
+          alt='' 
+        />
+      )}
+      <div className='postInfo'>
             <div className='postCats'>
-                <span className='postCat'>Tech</span>
-                <span className='postCat'>Jobs</span>
+              {post.categories.map((c)=> (
+                <span className='postCat'>{c.name}</span>
+              ))} 
             </div>
-            <span className='postTitle'>lorem lore lorem ipsum dolor sit amet, consectet</span> 
+            <span className='postTitle'>{post.title}</span> 
             <hr />
-            <span className='postDate'>1 hour ago</span>
+            <span className='postDate'>{new Date(post.createdAt).toDateString()}</span>
         </div>
         <p className='postDesc'>
-            lorem ipsum dolor sit amet, consect lorem
-            lorem ipsum dolor sit amet, consect lorem
-            lorem ipsum dolor sit amet, consect lorem
-            lorem ipsum dolor sit amet, consect lorem
-            lorem ipsum dolor sit amet, consect lorem
+          {post.desc}
         </p>
     </div>
   )
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../../components/hero/Hero'
 import Posts from '../../components/posts/Posts'
 import SideBar from '../../components/sidebar/SideBar'
 import './homepage.css'
+import axios from 'axios'
 
 const HomePage = () => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async() => {
+      const res = await axios.get('/posts')
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [])
   return (
     <>
       <Hero />
       <div className="homepage">
-        <Posts />
+        <Posts posts={posts} />
         <SideBar />
       </div>
     </>
